cf66e706b6049ec2b1663e614edad2797c14a2b6
Marcin Malek <marcinmalek01@gmail.com>
2022-03-04 22:46:22 +0100

    removed redundant return statement

--- src/Statement/index.js
+++ src/Statement/index.js
@@ -1,18 +1,15 @@
 import "./style.css";
 
-const Statement = ({ result }) => {
-    if (!!result) {
-        return (
-            <p className="statement">
-                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-                {" "}
-                <strong>
-                    {result.calculatedAmount.toFixed(2)}&nbsp;{result.currency}
-                </strong>
-            </p>
-        );
-    }
-    return null;
-}
+const Statement = ({ result }) => (
+    !!result && (
+        <p className="statement">
+            {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+            {" "}
+            <strong>
+                {result.calculatedAmount.toFixed(2)}&nbsp;{result.currency}
+            </strong>
+        </p>
+    )
+);
 
 export default Statement;
